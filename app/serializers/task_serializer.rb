07fb64f65821036c9d262eadcd6fa6a585f544b2
Shane Barringer <shane.barringer@outlook.com>
2016-12-08 10:26:42 -0500

    versions API and builds task resource

---
+++ app/serializers/task_serializer.rb
@@ -0,0 +1,8 @@
+class TaskSerializer < ActiveModel::Serializer
+
+  attributes :id, :name, :priority, :due_date
+
+  def due_date
+    object.due_date.strftime('%A, %b %d')
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  scope module: 'api' do
+    namespace :v1 do
+      resources :tasks
+    end
+  end
+  
+end
